de2adf551e2f7fee753241d9480bdf977900b471
Juliya <prakhova_yuliya@ukr.net>
2020-05-13 22:56:00 +0300

    news

--- js/main.js
+++ js/main.js
@@ -43,3 +43,13 @@ $(function(){
   prevArrow: '<button type="button" class="slick-btn slick-prev"><i class="fa fa-arrow-left" aria-hidden="true"></i></button>',
 });
 });
+
+var show;
+function hidetxt(type){
+ param=document.getElementById(type);
+ if(param.style.display == "none") {
+ if(show) show.style.display = "none";
+ param.style.display = "block";
+ show = param;
+ }else param.style.display = "none"
+}
